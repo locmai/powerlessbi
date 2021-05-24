8e6c48c3b81b3a37f87d690f4fdd303164948968
Loc Mai <locmai0201@gmail.com>
2021-05-25 01:32:06 +0700

    init commit

--- src/main.py
+++ src/main.py
@@ -1,17 +1,43 @@
 import click
 import yaml
-from simple_chalk import green, yellow
+from simple_chalk import green, yellow, red
 
-def upload():
+report_base_path = ''
+
+@click.command()
+@click.option('--target','-t', default=None, help='Targeting the team to publish report')
+def main(target):
+    """A powerless tool to help publishing to PowerBI service"""
+    
     data = {}
     with open('./src/config.yaml') as config:
         data = yaml.safe_load(config)
 
+    global report_base_path
+    report_base_path = data['report_folder_path']
     print(f"Processing for folder {data['report_folder_path']} ...\n")
 
-    print_reports(data['reports'])
+    
+    report_date = None
 
-    print(yellow("Proceed? (y/N)"))    
+    report_data = data['reports']
+
+    if target is not None:
+        pass
+    else:
+        print_reports(report_data)
+
+    proceed_answer = click.prompt(yellow("Proceed? (y/N)"), default='y')
+
+    if proceed_answer == 'y':
+        if target is not None:
+            pass
+        else:
+            upload_reports(data['reports'])
+    else:
+        print(red('Aborted!'))
+
+
 def print_reports(reports: dict) -> None:
     for key, value in reports.items():
         msg= f"""------------------
@@ -20,8 +46,25 @@ def print_reports(reports: dict) -> None:
 """
         print(msg)
         
-def main():
-    upload()
+
+def upload_reports(reports: dict) -> None:
+    for key, value in reports.items():        
+        login(value["username"],value["password"])   
+        report_files = value["files"]
+        for report_file in report_files:
+            upload(report_file)
+    print(green("Reports uploaded!"))
+
+
+def login(username: str, password: str):
+    print(f"Login with {username} and {password}")
+
+
+def upload(report_file: str):
+    print(f"Uploading {report_base_path}\\{report_file} ... ")
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(e)
